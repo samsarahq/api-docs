8a0590bc829c528f25064747423a01a33a502592
Advith Chelikani <advith.chelikani@samsara.com>
2021-01-07 14:14:24 -0800

    Update readme_orderer.py

--- scripts/readme_orderer.py
+++ scripts/readme_orderer.py
@@ -12,26 +12,62 @@ class BasicAuth(requests.auth.AuthBase):
         r.headers["Authorization"] = "Basic " + self.token
         return r
 
+# Read markdown files and store by slug.
+slugToBody = {}
+markdownFilePath = "../markdown"
+
+files = []
+for f in os.listdir(markdownFilePath):
+    print(f + "\n")
+    filePath = os.path.join(markdownFilePath, f)
+    if os.path.isfile(filePath):
+        files.append(filePath)
+        with open(filePath, 'r') as file:
+            # Slug is file name, without .md extension.
+            slugToBody[f[:-3]] = file.read()
+
 readme_auth = BasicAuth(README_API_TOKEN)
 
 response = requests.get('https://dash.readme.io/api/v1/categories/samsara-api/docs', auth=readme_auth)
 response_body = response.json()
 
+slugToDocTitle = {}
 tags = []
 for doc in response_body:
-    if "children" not in doc or doc["children"] == []:
-        continue
-    tags.append({
-        "title": doc["title"],
-        "slug": doc["slug"],
-    })
+    slugToDoc[doc["slug"]] = doc["title"]
+    if "children" in doc and doc["children"] != []:
+        tags.append({
+            "title": doc["title"],
+            "slug": doc["slug"],
+        })
+        for child in doc["children"]:
+            slugtoDocTitle[child["slug"]] = child["title"]
 
 tags = sorted(tags, key=lambda x: x["title"])
+
+# Update the markdown docs for all slugs.
+print("Updating markdown for all docs...\n")
+for slug in slugToBody:
+    body = slugToBody[slug]
+    if slug not in slugToDocTitle:
+        print(slug + " has associated markdown file, but was not found in readme")
+        continue
+    title = slugToDocTitle[slug]
+
+    payload = {
+        "title": title,
+        "category": README_CATEGORY_ID,
+        "body": body,
+    }
+
+    resp = requests.put("https://dash.readme.io/api/v1/docs/" + tag["slug"], auth=readme_auth, data=payload)
+    print(slug + ": " + str(resp.status_code) + "\n")
 
 # Start at order=1, because overview is at order=0.
 order = 1
 previewApi = None
 betaApi = None
+print("Reordering sidebar tags...\n")
 for tag in tags:
     if tag["title"] == "Preview APIs":
         previewApi = tag
@@ -46,7 +82,7 @@ for tag in tags:
         "order": order,
     }
     resp = requests.put("https://dash.readme.io/api/v1/docs/" + tag["slug"], auth=readme_auth, data=payload)
-    print(resp.status_code)
+    print(tag["slug"] + ": " + str(resp.status_code) + "\n")
     order += 1
 
 # Add Beta APIs and Preview APIs at the end.
@@ -57,7 +93,7 @@ if betaApi:
         "order": order,
     }
     resp = requests.put("https://dash.readme.io/api/v1/docs/" + betaApi["slug"], auth=readme_auth, data=payload)
-    print(resp.status_code)
+    print(betaApi["slug"] + ": " + str(resp.status_code) + "\n")
     order += 1
 
 if previewApi:
@@ -67,5 +103,5 @@ if previewApi:
         "order": order,
     }
     resp = requests.put("https://dash.readme.io/api/v1/docs/" + previewApi["slug"], auth=readme_auth, data=payload)
-    print(resp.status_code)
+    print(previewApi["slug"] + ": " + str(resp.status_code) + "\n")
     order += 1
